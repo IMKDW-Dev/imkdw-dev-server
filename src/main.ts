4aadf077512ce6167660862626c4674d2bf5c306
imkdw <imkdw@kakao.com>
2024-05-20 00:18:11 +0900

    feat: cors 도메인 추가

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', "https://imkdw.dev"],
     credentials: true,
   });
 
