27e802932ace929e3f55dfd4146082e2a86be693
imkdw <imkdw@kakao.com>
2024-05-25 14:58:40 +0900

    feat: prisma soft delete 활성화

--- src/infra/database/prisma.ts
+++ src/infra/database/prisma.ts
@@ -1,34 +1,73 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { PrismaClient } from '@prisma/client';
 import { ILocalStorageService } from '../local-storage/interfaces/local-storage.interface';
 
 export const PRISMA_SERVICE = 'PRISMA_SERVICE';
 const prismaClient = new PrismaClient({});
-export const extendedPrismaClient = (localStorageService: ILocalStorageService) =>
-  prismaClient.$extends({
+export const extendedPrismaClient = (localStorageService: ILocalStorageService) => {
+  const userId = localStorageService.getUserId() ?? '_system';
+
+  return prismaClient.$extends({
     query: {
       $allModels: {
-        async $allOperations({ query, operation, args }) {
-          const userId = localStorageService.getUserId() ?? '_system';
-          const tempArgs = { ...args };
-
-          if (operation === 'create') {
-            Object.assign(tempArgs, { ...args, data: { ...args.data, createUser: userId, updateUser: userId } });
-          }
-
-          if (operation === 'update') {
-            Object.assign(tempArgs, { ...args, data: { ...args.data, updateUser: userId } });
-          }
-
-          // if (operation === 'delete' || operation === 'deleteMany') {
-          //   // eslint-disable-next-line no-param-reassign
-          //   Object.assign(tempArgs, { ...args, data: { deleteAt: new Date() } });
-          //   return query(tempArgs);
-          // }
-
-          return query(tempArgs);
+        async findUnique({ model, args }) {
+          return (prismaClient as any)[model].findUnique({
+            ...args,
+            where: { ...args.where, deleteAt: null },
+          });
+        },
+        async findFirst({ model, args }) {
+          return (prismaClient as any)[model].findFirst({
+            ...args,
+            where: { ...args.where, deleteAt: null },
+          });
+        },
+        async findMany({ model, args }) {
+          return (prismaClient as any)[model].findMany({
+            ...args,
+            where: { ...args.where, deleteAt: null },
+          });
+        },
+        async create({ model, args }) {
+          return (prismaClient as any)[model].create({
+            ...args,
+            data: { ...args.data, createUser: userId, updateUser: userId },
+          });
+        },
+        async createMany({ model, args }) {
+          return (prismaClient as any)[model].createMany({
+            ...args,
+            data: { ...args.data, createUser: userId, updateUser: userId },
+          });
+        },
+        async update({ model, args }) {
+          return (prismaClient as any)[model].update({
+            ...args,
+            data: { ...args.data, updateUser: userId },
+          });
+        },
+        async updateMany({ model, args }) {
+          return (prismaClient as any)[model].updateMany({
+            ...args,
+            data: { ...args.data, updateUser: userId },
+          });
+        },
+        async delete({ model, args }) {
+          return (prismaClient as any)[model].update({
+            ...args,
+            data: { deleteAt: new Date(), deleteUser: userId },
+          });
+        },
+        async deleteMany({ model, args }) {
+          return (prismaClient as any)[model].updateMany({
+            ...args,
+            data: { deleteAt: new Date(), deleteUser: userId },
+          });
         },
       },
     },
   });
+};
 
 export interface ExtendedPrismaClient extends PrismaClient {}
