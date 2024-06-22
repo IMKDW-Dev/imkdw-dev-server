827f742a89c03d0f42263e25e0d57be3e2e83b11
imkdw <imkdw@kakao.com>
2024-06-22 22:55:59 +0900

    refactor: oauth provider 제거

--- src/infra/http/rest/interfaces/http-rest.interface.ts
+++ src/infra/http/rest/interfaces/http-rest.interface.ts
@@ -3,7 +3,7 @@ import { AxiosRequestConfig } from 'axios';
 
 export const HTTP_REST_SERVICE: InjectionToken = Symbol('HTTP_REST_SERVICE');
 
-export interface HttpRestService {
+export interface IHttpRestService {
   get<T>(url: string, options?: AxiosRequestConfig): Promise<T>;
   post<T>(url: string, body: unknown, options?: AxiosRequestConfig): Promise<T>;
 }
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 
 import UserModule from '../user/user.module';
-import GoogleOAuthProvider from './providers/google-oauth.provider';
 import HttpRestModule from '../../infra/http/rest/http-rest.module';
 import OAuthService from './services/oauth.service';
 import OAuthController from './controllers/oauth.controller';
-import KakaoOAuthProvider from './providers/kakao-oauth.provider';
-import GithubOAuthProvider from './providers/github-oauth.provider';
 import AuthService from './services/auth.service';
 import CookieModule from '../../infra/secure/cookie/cookie.module';
 import AuthController from './controllers/auth.controller';
@@ -15,6 +12,6 @@ import TokenModule from '../token/token.module';
 @Module({
   imports: [UserModule, HttpRestModule, CookieModule, TokenModule],
   controllers: [OAuthController, AuthController],
-  providers: [OAuthService, GoogleOAuthProvider, KakaoOAuthProvider, GithubOAuthProvider, AuthService],
+  providers: [OAuthService, AuthService],
 })
 export default class AuthModule {}
