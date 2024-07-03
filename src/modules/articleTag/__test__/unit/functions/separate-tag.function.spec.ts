90b9c3770dea9e2c02a5494d96fdb5ac4f27cc1b
imkdw <imkdw@kakao.com>
2024-07-03 20:45:41 +0900

    fix: 게시글 작성시 새로운 태그가 생성되지 않는 버그 수정

--- src/modules/articleTag/__test__/unit/functions/separate-tag.function.spec.ts
+++ src/modules/articleTag/__test__/unit/functions/separate-tag.function.spec.ts
@@ -5,12 +5,12 @@ describe('getNewTags', () => {
   it('새로운 태그이름들을 반환한다', () => {
     // Given
     const existTags = Array.from({ length: 5 }, (_, i) => new Tag.builder().setName(`태그${i + 1}`).build());
-    const newTagNames = ['태그2', '태그4', '태그5'];
+    const newTagNames = ['태그2', '태그6', '태그7'];
 
     // When
-    const sut = getNewTags(existTags, newTagNames);
+    const result = getNewTags(existTags, newTagNames);
 
     // Then
-    expect(sut).toEqual(['태그1', '태그3']);
+    expect(result).toEqual(['태그6', '태그7']);
   });
 });
